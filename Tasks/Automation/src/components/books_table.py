a5c56a6640e7b8cfe5a3a524cad47afe1755eb95
Andriy Kozachok <andrew.kozachok7@gmail.com>
2023-04-02 22:02:32 +0300

    Update search_books_in_table

--- Tasks/Automation/src/components/books_table.py
+++ Tasks/Automation/src/components/books_table.py
@@ -10,22 +10,15 @@ class BooksTable(BookStorePage):
         self.filter_field_name = None
         self.table = s(by.xpath("//div[@class='ReactTable -striped -highlight']"))
         self.table_items = ss(by.xpath("//div[@class='rt-tr-group']"))
-        self.table_items_title = ss(by.xpath("//div[@role='gridcell']/descendant::a"))
+        self.table_items_title = ss(by.xpath("//span[@class='mr-2']//a"))
         self.table_items_author = ss(by.xpath("//div[@class='rt-tr-group']/descendant::div[@class='rt-td'][3]"))
         self.table_items_publisher = ss(by.xpath("//div[@class='rt-tr-group']/descendant::div[@class='rt-td'][3]"))
 
-    def search_books_in_table(self, prompt: str) -> Dict[str, list]:
+    def search_books_in_table(self, prompt: str):
         self.enter_request(prompt)
-        books = {}
-        for author, title in zip(self.table_items_author, self.table_items_title):
-            if isinstance(author, WebElement):
-                author_name = author.get_attribute('text')
-                title_name = title.get_attribute('text')
-                if author_name in books:
-                    books[author_name].append(title_name)
-                else:
-                    books[author_name] = [title_name]
-        return books
+        book_titles = []
+        for book in self.table_items_title:
+            book_titles.append(book.text)
 
     def filter_book_by_filter_name(self, filter_name: str):
         self.filter_field_name = s(by.xpath(f"// div[contains(text(), '{filter_name}')]")).click()
