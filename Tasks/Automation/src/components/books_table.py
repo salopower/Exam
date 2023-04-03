8b3fecb3c5d6e570deaee0d71c27b0ea1d308f75
Andriy Kozachok <andrew.kozachok7@gmail.com>
2023-04-03 17:39:34 +0300

    Add new methods for searching authors and publishers

--- Tasks/Automation/src/components/books_table.py
+++ Tasks/Automation/src/components/books_table.py
@@ -1,6 +1,5 @@
 from selene.api import *
-from typing import Dict
-from selenium.webdriver.remote.webelement import WebElement
+from typing import Dict, List, Set
 from Tasks.Automation.src.pages.BookStorePage import BookStorePage
 
 
@@ -10,11 +9,32 @@ class BooksTable(BookStorePage):
         self.filter_field_name = None
         self.table = s(by.xpath("//div[@class='ReactTable -striped -highlight']"))
         self.table_items = ss(by.xpath("//div[@class='rt-tr-group']"))
+        self.table_rows = ss(by.xpath("//div[@role='rowgroup']"))
         self.table_items_title = ss(by.xpath("//span[@class='mr-2']//a"))
-        self.table_items_author = ss(by.xpath("//div[@class='rt-tr-group']/descendant::div[@class='rt-td'][3]"))
-        self.table_items_publisher = ss(by.xpath("//div[@class='rt-tr-group']/descendant::div[@class='rt-td'][3]"))
+        self.table_items_author = ss(by.xpath("//div[@class='rt-td'][3]"))
+        #  //div[@class='rt-td'][3]/text()
+        self.table_items_publisher = ss(by.xpath("//div[@class='rt-td'][4]"))
+        #  //div[@class='rt-td'][4]/text()
 
-    def search_books_in_table(self, prompt: str):
+    def get_unique_values(self, prompt: str, attribute: str) -> Set[str]:
+        self.enter_request(prompt)
+        if attribute == 'author':
+            elements = self.table_items_author
+        elif attribute == 'publisher':
+            elements = self.table_items_publisher
+        else:
+            raise ValueError(f'Invalid attribute value: {attribute}')
+        values = (elem.get(query.text) for elem in elements if
+                  elem.get(query.text) != '' and elem.get(query.text) != ' ')
+        unique_values = set(set(values))
+        return unique_values
+
+    def get_author_and_publisher(self, prompt: str) -> Dict[str, Set[str]]:
+        author_values = self.get_unique_values(prompt, 'author')
+        publisher_values = self.get_unique_values(prompt, 'publisher')
+        return {'author': author_values, 'publisher': publisher_values}
+
+    def search_books_in_table(self, prompt: str) -> List[str]:
         self.enter_request(prompt)
         book_titles = []
         for book in self.table_items_title:
@@ -22,6 +42,21 @@ class BooksTable(BookStorePage):
         if not book_titles:
             raise Exception("No books found")
         return book_titles
+
+    def search_books(self, prompt: str) -> dict[str, dict[str, list[str]]]:
+        self.enter_request(prompt)
+        books = {}
+        for book in self.table_items_title:
+            book_title = book.get(query.text)
+            authors = (elem.get(query.text) for elem in self.table_items_author if
+                       elem.get(query.text) != '' and elem.get(query.text) != ' ')
+            author = list(set(authors))
+            publishers = (elem.get(query.text) for elem in self.table_items_publisher if
+                          elem.get(query.text) != '' and elem.get(query.text) != ' ')
+            publisher = list(set(publishers))
+            if book_title and author and publisher:
+                books[book_title] = {'author': author, 'publisher': publisher}
+        return books
 
     def filter_book_by_filter_name(self, filter_name: str):
         self.filter_field_name = s(by.xpath(f"// div[contains(text(), '{filter_name}')]")).click()
--- Tasks/Automation/tests/test_book_store.py
+++ Tasks/Automation/tests/test_book_store.py
@@ -14,10 +14,32 @@ class TestBookStore:
         BookStorePage().enter_request(git_book.title)
 
     def test_get_books_from_table(self, bookstore):
-        book = Book(publisher='No Starch Press')
+        book = Book(publisher="O'Reilly Media")
         books_table = BooksTable()
         books = books_table.search_books_in_table(book.publisher)
-        assert isinstance(books, dict)
+        for book in books:
+            print(f'\n{book}')
+
+        assert isinstance(books, list)
 
     def test_filter_table(self, bookstore):
-        BooksTable().filter_book_by_filter_name('publisher')
+        BooksTable().filter_book_by_filter_name('Publisher')
+
+    def test_get_publisher(self, bookstore):
+        books_table = BooksTable()
+        publishers = books_table.get_unique_values('Git Pocket Guide', 'author')
+        print(f'\n{publishers}')
+
+    def test_get_author_and_publisher(self, bookstore):
+        books_table = BooksTable()
+        unique_values = books_table.get_author_and_publisher('No Starch Press')
+        print(unique_values)
+
+    def test_search_books_by_prompt(self, bookstore):
+        books_table = BooksTable()
+        books = books_table.search_books("No Starch Press")
+        for book_title, book_info in books.items():
+            print(f'\n{book_title}')
+            for key, value in book_info.items():
+                print(f'{key}: {value}')
+        assert len(books) > 0
